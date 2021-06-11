0660e68c88f5faa8e764799f52d41263a83cb2e0
Mahmood Tahir <mahmood.tahir@thescore.com>
2021-06-11 09:34:06 -0400

    Update changelog

--- Sources/Nimble_Snapshots/Nimble_Snapshots.h
+++ Sources/Nimble_Snapshots/Nimble_Snapshots.h
@@ -8,4 +8,8 @@ FOUNDATION_EXPORT const unsigned char Nimble_SnapshotsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Nimble_Snapshots/PublicHeader.h>
 
+#if __has_include("NBSMockedApplication.h")
+#import "NBSMockedApplication.h"
+#else
 #import <Nimble_Snapshots/NBSMockedApplication.h>
+#endif
